529ca0812cc9f231a68997581776ce9b744353b0
Rajiv <kumarrajiv0920@gmail.com>
2026-07-14 13:46:03 +0530

    Add health route to active the API

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,40 +1,53 @@
-import express from "express";
-import authRoutes from "./routes/auth.route.js";
-import messageRoutes from "./routes/message.route.js";
-import { connectDB } from "./lib/db.js";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import 'dotenv/config'
-import { app, server } from "./lib/socket.js";
-import path from "path";
-
+import express from 'express';
+import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
+import { connectDB } from './lib/db.js';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import 'dotenv/config';
+import { app, server } from './lib/socket.js';
+import path from 'path';
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
 app.use(express.json({ limit: '20mb' }));
 app.use(cookieParser());
+
+const allowedOrigins = ['http://localhost:5173'];
+
 app.use(
-    cors({
-        origin: "http://localhost:5173",
-        credentials: true,
-    })
+  cors({
+    origin: function (origin, callback) {
+      // allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          'The CORS policy for this site does not allow access from the specified Origin.';
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  }),
 );
 
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
+app.get('/health', (req, res) => {
+  res.send('OK');
+});
 
-app.use("/api/auth", authRoutes);
-app.use("/api/messages", messageRoutes)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
-
-    // app.get("*", (req, res) => {
-    app.get("/files{/*path}", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    })
+  // app.get("*", (req, res) => {
+  app.get('/files{/*path}', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+  });
 }
 
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    connectDB();
-})
+  console.log(`Server is running on port ${PORT}`);
+  connectDB();
+});
