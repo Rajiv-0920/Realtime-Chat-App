8edaea07d358ac129b3fd07c41bfc3bb40ee8668
Rajiv <kumarrajiv0920@gmail.com>
2026-07-14 14:31:53 +0530

    Fix: same site secure

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,15 +1,15 @@
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 export const generateToken = (userId, res) => {
-    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
-        expiresIn: "7d"
-    })
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: '7d',
+  });
 
-    res.cookie("jwt", token, {
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-        httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-        sameSite: "strict",
-        secure: process.env.NODE_ENV !== "development"
-    })
+  res.cookie('jwt', token, {
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true, // prevent XSS attacks cross-site scripting attacks
+    sameSite: process.env.NODE_ENV === 'development' ? 'lax' : 'none', // 🔥 Changed from "strict"
+    secure: process.env.NODE_ENV !== 'development', // Required if sameSite is "none"
+  });
 
-    return token;
-}
+  return token;
+};
